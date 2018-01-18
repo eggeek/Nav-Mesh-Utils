749790124c8d6f10827097ec9ca9078b3d8823ab
eggeek <eggeek.me@gmail.com>
2018-01-19 09:55:05 +1100

    update py script

--- scripts/parse_polys.py
+++ scripts/parse_polys.py
@@ -4,7 +4,7 @@ import math
 def dist(p1, p2):
     return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
-def parse_poly(line):
+def parse_poly_csv(line):
     cols = line.split(',')
     ps = []
     for i in cols[1:]:
@@ -21,7 +21,7 @@ def parse(fname):
         raw_lines = f.readlines()
     polys = []
     for line in raw_lines:
-        poly = parse_poly(line)
+        poly = parse_poly_csv(line)
         if (len(poly) >= 3):
             polys.append(poly)
     return polys
@@ -43,6 +43,30 @@ def rescale(polys):
         for i in range(len(poly)):
             poly[i] = (poly[i][0] * scale, poly[i][1] * scale)
     return polys
+
+def read_poly_file(fname):
+  with open(fname, "r") as f:
+    rlines = f.readlines()
+  polys = []
+  for rline in rlines[3:]:
+    line = [int(i) for i in rline.split()]
+    n = line[0]
+    poly = []
+    for i in range(n):
+      poly.append((line[1 + 2*i], line[1+2*i+1]))
+    polys.append(poly)
+  return polys
+
+def get_xrange(poly):
+  min_x = min([i[0] for i in poly])
+  max_x = max([i[0] for i in poly])
+  return (min_x, max_x)
+
+def get_yrange(poly):
+  min_y = min([i[1] for i in poly])
+  max_y = max([i[1] for i in poly])
+  return (min_y, max_y)
+
 
 def main(fname):
     polys = []
